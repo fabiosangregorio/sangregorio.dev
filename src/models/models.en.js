cdf3962f46b7b9fdf11f2ab81e23be86d1062078
Fabio <fabio.sangregorio@gmail.com>
2021-06-11 11:35:48 +0200

    Add website

--- src/models/models.en.js
+++ src/models/models.en.js
@@ -1,6 +1,28 @@
 module.exports = {
   meta: {
     title: "Sangregorio Brothers",
-    description: ""
-  }
+    description: "We're two brothers both in the field of Software Engineering. This is the home hub for our websites."
+  },
+  header: "Which Sangregorio are you looking for?",
+  footer: "(yeah, I know, we're both software engineers...)",
+  people: [
+    {
+      url: "https://paolo.sangregorio.dev",
+      urlText: "paolo.sangregorio.dev",
+      image: "/images/paolo.png",
+      name: "Paolo Sangregorio",
+      role: "Software Engineer III at Google",
+      birthday: new Date(1989, 6, 8),
+      location: "Dublin, Republic of Ireland"
+    },
+    {
+      url: "https://fabio.sangregorio.dev",
+      urlText: "fabio.sangregorio.dev",
+      image: "/images/fabio.png",
+      name: "Fabio Sangregorio",
+      role: "Security Research Intern at SAP",
+      birthday: new Date(1997, 10, 14),
+      location: "Milan, Italy"
+    }
+  ]
 }
