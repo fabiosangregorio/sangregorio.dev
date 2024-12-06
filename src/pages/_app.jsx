4e63f882aee11bff125b08e4bb20dd28e809f5b5
Fabio Sangregorio <fabs@bendingspoons.com>
2024-12-06 12:41:31 +0100

    refactor: create model file

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,21 +1,15 @@
-import '../sass/app.sass'
-import { Open_Sans } from 'next/font/google'
-import Head from 'next/head'
+import '../sass/app.sass';
+import { Open_Sans } from 'next/font/google';
 
 const openSans = Open_Sans({
   subsets: ['latin'],
   display: 'swap',
-})
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <link rel="icon" href="data:," />
-      </Head>
-      <main className={openSans.className}>
-        <Component {...pageProps} />
-      </main>
-    </>
-  )
+    <main className={openSans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 } 
