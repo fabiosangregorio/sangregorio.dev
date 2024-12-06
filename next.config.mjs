30493a6223e63c9e8d1ef100f4583ac0b01573f6
fabiosangregorio <fabio.sangregorio@gmail.com>
2024-12-06 10:32:26 +0000

    Deploying to gh-pages from @ fabiosangregorio/sangregorio.dev@d272b00b2bf4026c046deb4dfc0ef7d6bc75fe68 🚀

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: '/',
+  basePath: '',
+  trailingSlash: true,
+};
+
+export default nextConfig; 
